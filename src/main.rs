70612e01936c4004c90f4e2572217a1e87beed34
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-08-08 01:46:23 +0300

    derive a library to export server runner

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/healthz", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
 }
