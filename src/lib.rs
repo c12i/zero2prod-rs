12490b0b02424665f806d063d63ffbd38f950e3e
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-09-22 03:14:10 +0300

    Make health check endpoint return Ok

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().body("Ok")
 }
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/healthz", web::get().to(health_check)))
