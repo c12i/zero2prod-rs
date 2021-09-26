8e8340ba6f9805d2bf5960ed0b11de9936765c82
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-09-27 02:54:20 +0300

    Refactor server base to accept TCP listener instance and update main and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body("Ok")
 }
-pub fn run(address: &str) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/healthz", web::get().to(health_check)))
-        .bind(address)?
+        .listen(listener)?
         .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,20 @@
-fn spawn_app() {
-    let server = null_to_prod::run("127.0.0.1:0").expect("Cannot start server");
+use std::net::TcpListener;
+
+// spawn app and return bound TCP address
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Cannot bind to TCP listener");
+    let port = listener.local_addr().unwrap().port();
+    let server = null_to_prod::run(listener).expect("Cannot start server");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[actix_rt::test]
-async fn health_check_works() {
-    spawn_app();
-
+async fn healtz_works() {
+    let address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-        .get("http://127.0.0.1:8000/healthz")
+        .get(&format!("{}/healthz", &address))
         .send()
         .await
         .expect("Failed to execute request");
