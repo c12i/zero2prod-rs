32b6687e21c4e25582a48ab846e854dab4d327a5
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-10-01 01:48:39 +0300

    Add failing tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,3 +21,45 @@ async fn healtz_works() {
     assert_eq!(response.status(), reqwest::StatusCode::OK);
     assert_eq!(Some(2), response.content_length());
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=urslula_leguin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Filed to execute request");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("name=urslula_leguin%40gmail", "missing name"),
+        ("", "missing both name and email")
+    ];
+    for (invalid_body, error_message) in test_cases {
+        // act
+        let respose = client
+            .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(invalid_body)
+        .send()
+        .await
+        .expect("Filed to execute request");
+        assert_eq!(
+            400,
+            respose.status().as_u16(),
+            "The API did not fail with a 400 request when payload was {}",
+            error_message
+        );
+    }
+}
