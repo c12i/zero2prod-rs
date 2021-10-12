b05bb83b371db37afb0826a3dbf8af19064a9726
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-10-13 01:44:45 +0300

    Replace env_logger with tracing subscriber and tracing subscriber layers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use env_logger::Env;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use sqlx::PgPool;
 use z2p::{get_configuration, run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // `init` calls `set_logger`, all that is needed
     // fall back to printing all logs at inflo level or above
     // if RUST_LOG env var has not been set
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("z2p".into(), std::io::stdout);
+    // the `with` method is provided by `SubsciberExt`, an extension trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    // `set_global_default` can be used by applications to specify what subscriber should be used to process spans
+    set_global_default(subscriber).expect("Error getting subscriber");
     let config = get_configuration().expect("Error reading configurations");
     let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application_port))?;
     let db_connection_pool = PgPool::connect(&config.database.get_connection_string())
