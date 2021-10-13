24320f278e639c3339a9c14417da5764f2ad45d8
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-10-13 22:39:25 +0300

    Derive a get and initialize subsdriber function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
+use tracing::{Subscriber, subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-use sqlx::PgPool;
 use z2p::{get_configuration, run};
+
+/// Compose multiple layers into a `tracingd's` suscriber
+///
+/// ## Implementation Notes
+/// We return `impl Subscriber` to avoid having to spell out the actual type
+/// of the returned type which can be complx
+/// We also need to call out that the returned trait object also implements
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    // fall back to printing all logs at inflo level or above
+    // if RUST_LOG env var has not been set
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    // the `with` method is provided by `SubsciberExt`, an extension trait for `Subscriber` exposed by `tracing_subscriber`
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as a global default to process span data
+/// 
+/// Should only be called once
+pub fn initialize_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    // redirect all Actix web `log` events to subscriber
+    LogTracer::init().expect("Error setting logger");
+    // `set_global_default` can be used by applications to specify what subscriber should be used to process spans
+    set_global_default(subscriber).expect("Error getting subscriber");
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // fall back to printing all logs at inflo level or above
-    // if RUST_LOG env var has not been set
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("z2p".into(), std::io::stdout);
-    // the `with` method is provided by `SubsciberExt`, an extension trait for `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify what subscriber should be used to process spans
-    set_global_default(subscriber).expect("Error getting subscriber");
-    // redirect all Actix web `log` events to subscriber
-    LogTracer::init().expect("Error setting logger");
+    let subscriber = get_subscriber("z2p".into(), "info".into());
+    initialize_subscriber(subscriber);
     let config = get_configuration().expect("Error reading configurations");
     let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application_port))?;
     let db_connection_pool = PgPool::connect(&config.database.get_connection_string())
