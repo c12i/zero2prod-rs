a599effb7c7d85ea31e129f0a58f801460ae9343
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-10-21 02:33:30 +0300

    Use lazy sqlx connection pool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use z2p::configuration::get_configuration;
 use z2p::run;
@@ -10,7 +10,9 @@ async fn main() -> std::io::Result<()> {
     initialize_subscriber(subscriber);
     let config = get_configuration().expect("Error reading configurations");
     let listener = TcpListener::bind(format!("127.0.0.1:{}", config.application.port))?;
-    let db_connection_pool = PgPool::connect(&config.database.get_connection_string())
+    let db_connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&config.database.get_connection_string())
         .await
         .expect("Error connecting to Postgres");
     run(listener, db_connection_pool)?.await
