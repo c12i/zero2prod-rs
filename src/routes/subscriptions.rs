74031e4dd4e72128cd64eef1519a891b872e8b1c
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-12-11 17:11:21 +0300

    Remove unused import

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 
 use crate::domain::{NewSubscriber, SubscriberName};
 
