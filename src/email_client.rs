1e6159d7a7f24652a10eb1c77bea637cd6b8aad9
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-12-30 22:39:10 +0300

    Add send email 500 status test case

--- src/email_client.rs
+++ src/email_client.rs
@@ -49,7 +49,8 @@ impl EmailClient {
             .header("X-Postmark-Server-Token", &self.authorization_token)
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
         Ok(())
     }
 }
@@ -58,7 +59,7 @@ impl EmailClient {
 mod test {
     use crate::domain::SubscriberEmail;
     use crate::email_client::EmailClient;
-    use claim::assert_ok;
+    use claim::{assert_err, assert_ok};
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
@@ -134,5 +135,27 @@ mod test {
             .await;
         // Assert
         assert_ok!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Faker.fake());
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+        Mock::given(any())
+            // Not a 200 anymore!
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+        // Assert
+        assert_err!(outcome);
     }
 }
