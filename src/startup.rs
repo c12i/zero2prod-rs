4ebdc18b2aa673a2522b023608c486d156db042b
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-01-04 08:37:50 +0300

    Clean up imports

--- src/startup.rs
+++ src/startup.rs
@@ -4,10 +4,8 @@ use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
-use crate::{
-    email_client::EmailClient,
-    routes::{health_check, subscribe},
-};
+use crate::email_client::EmailClient;
+use crate::routes::{health_check, subscribe};
 
 fn not_found() -> HttpResponse {
     HttpResponse::NotFound().finish()
