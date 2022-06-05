26636a26e18e8966e241f99592729e604fb3b066
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-06-06 00:31:18 +0300

    Remove context trait import

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,6 +1,11 @@
-use actix_web::{web, HttpResponse};
+use std::fmt::Debug;
+
+use actix_http::StatusCode;
+use actix_web::{web, HttpResponse, ResponseError};
 use serde::Deserialize;
 use sqlx::PgPool;
+
+use crate::utils::error_chain_fmt;
 
 #[derive(Deserialize)]
 pub struct Parameters {
@@ -12,20 +17,16 @@ pub struct Parameters {
 	skip(parameters)
 )]
 #[allow(clippy::async_yields_async)]
-pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
-    let result = match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
-        Ok(id) => id,
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    };
-    match result {
-        Some(subscriber_id) => {
-            if confirm_subscriber(&pool, subscriber_id).await.is_err() {
-                return HttpResponse::InternalServerError().finish();
-            }
-            HttpResponse::Ok().finish()
-        }
-        None => HttpResponse::Unauthorized().finish(),
+pub async fn confirm(
+    parameters: web::Query<Parameters>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, SubscriptionConfirmError> {
+    let result = get_subscriber_id_from_token(&pool, &parameters.subscription_token).await?;
+    if let Some(subscriber_id) = result {
+        confirm_subscriber(&pool, subscriber_id).await?;
+        return Ok(HttpResponse::Ok().finish());
     }
+    Ok(HttpResponse::Unauthorized().finish())
 }
 
 #[tracing::instrument(
@@ -35,7 +36,7 @@ pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>
 pub async fn get_subscriber_id_from_token(
     pool: &PgPool,
     subscription_token: &str,
-) -> Result<Option<uuid::Uuid>, sqlx::Error> {
+) -> Result<Option<uuid::Uuid>, SubscriptionConfirmError> {
     let result = sqlx::query!(
         r#"
 		SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1
@@ -43,7 +44,8 @@ pub async fn get_subscriber_id_from_token(
         subscription_token
     )
     .fetch_optional(pool)
-    .await?;
+    .await
+    .map_err(SubscriptionConfirmError::UnexpectedError)?;
     Ok(result.map(|r| r.subscriber_id))
 }
 
@@ -51,7 +53,7 @@ pub async fn get_subscriber_id_from_token(
 pub async fn confirm_subscriber(
     pool: &PgPool,
     subscriber_id: uuid::Uuid,
-) -> Result<(), sqlx::Error> {
+) -> Result<(), SubscriptionConfirmError> {
     sqlx::query!(
         r#"
 			UPDATE subscriptions SET status = 'confirmed' WHERE id = $1
@@ -59,6 +61,27 @@ pub async fn confirm_subscriber(
         subscriber_id,
     )
     .execute(pool)
-    .await?;
+    .await
+    .map_err(SubscriptionConfirmError::UnexpectedError)?;
     Ok(())
+}
+
+#[derive(thiserror::Error)]
+pub enum SubscriptionConfirmError {
+    #[error("An error has occurred: {0}")]
+    UnexpectedError(#[from] sqlx::Error),
+}
+
+impl Debug for SubscriptionConfirmError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for SubscriptionConfirmError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            SubscriptionConfirmError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
