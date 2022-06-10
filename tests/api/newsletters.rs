9221c653f0cd76d3a84acb38295d544752c1a21d
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-06-10 04:00:12 +0300

    Derive a helper method to post newsletters and update usage in test cases

--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -20,12 +20,7 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
             "html": "<p>Newsletter body as plain HTML</p>"
         }
     });
-    let response = reqwest::Client::new()
-        .post(&format!("{}/newsletters", &app.address))
-        .json(&newsletter_request_body)
-        .send()
-        .await
-        .expect("Failed to execute request");
+    let response = app.post_newsletters(newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 200)
 }
 
@@ -47,12 +42,7 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
             "html": "<p>Newsletter body as plain HTML</p>"
         }
     });
-    let response = reqwest::Client::new()
-        .post(&format!("{}/newsletters", &app.address))
-        .json(&newsletter_request_body)
-        .send()
-        .await
-        .expect("Failed to execute request");
+    let response = app.post_newsletters(newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 200)
 }
 
@@ -77,13 +67,7 @@ async fn newsletters_returns_400_for_invalid_data() {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        let response = reqwest::Client::new()
-            .post(&format!("{}/newsletters", &app.address))
-            .json(&invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
-
+        let response = app.post_newsletters(invalid_body).await;
         // Assert
         assert_eq!(
             400,
