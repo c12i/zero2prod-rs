907e3024454514489d53897102892ecf9bd82137
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-07-03 02:31:12 +0300

    Update login post response type

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -2,5 +2,7 @@ use actix_http::header::LOCATION;
 use actix_web::HttpResponse;
 
 pub fn login() -> HttpResponse {
-    HttpResponse::Ok().insert_header((LOCATION, "/")).finish()
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish()
 }
