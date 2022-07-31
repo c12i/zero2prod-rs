5e0104743380b3c854b5b2ef426a33a0c777b33a
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-08-01 01:29:33 +0300

    Fix clippy ci error

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,10 +1,11 @@
 use actix_web::{http::header::ContentType, HttpResponse};
 use actix_web_flash_messages::{IncomingFlashMessages, Level};
+use std::fmt::Write;
 
 pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
     let mut error_html = String::new();
     for m in flash_messages.iter().filter(|m| m.level() == Level::Error) {
-        error_html.push_str(&format!("<p><i>{}</i></p>", m.content()));
+        write!(&mut error_html, "<p><i>{}</i></p>", m.content()).unwrap();
     }
     let response = HttpResponse::Ok()
         .content_type(ContentType::html())
