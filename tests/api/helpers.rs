97287f3b33bc73be7e671428c0d4c44ea3ac370b
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-08-03 05:54:31 +0300

    Remove debug macro call in test helper

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -189,7 +189,6 @@ impl TestUser {
         .hash_password(self.password.as_bytes(), &salt)
         .unwrap()
         .to_string();
-        dbg!(&password_hash);
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash) VALUES ($1, $2, $3)",
             self.user_id,
