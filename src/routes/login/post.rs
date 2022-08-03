f7cbbe89f7175b175d072895436da5293398eeaf
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-08-03 05:03:05 +0300

    Derive a custom typed session struct

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,9 +1,9 @@
-use actix_session::Session;
 use actix_web::http::header::LOCATION;
 use actix_web::{error::InternalError, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use sqlx::PgPool;
 
+use crate::session_state::TypedSession;
 use crate::{
     authentication::{validate_credentials, AuthError, Credentials},
     utils::error_chain_fmt,
@@ -16,7 +16,7 @@ use crate::{
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -27,7 +27,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::AuthError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
