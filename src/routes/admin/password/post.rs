2439ee8be2108ed00655934138274b7dfe842bc2
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-08-08 00:02:03 +0300

    Validate current password on update

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,17 +1,23 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
+use crate::authentication::{validate_credentials, AuthError, Credentials};
+use crate::routes::admin::dashboard::get_username;
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
+    let user_id = session.get_user_id().map_err(e500)?;
+    if user_id.is_none() {
         return Ok(see_other("/login"));
     };
+    let user_id = user_id.unwrap();
     // `Secret<String>` does not implement `Eq`,
     // therefore we need to compare the underlying `String`.
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
@@ -21,12 +27,28 @@ pub async fn change_password(
         .send();
         return Ok(see_other("/admin/password"));
     }
-    todo!()
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password.expose_secret().to_string(),
+    };
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentialsError(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e).into()),
+        };
+    } else {
+        // FIXME
+        unimplemented!()
+    }
 }
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    current_password: Secret<String>,
-    new_password: Secret<String>,
-    new_password_check: Secret<String>,
+    pub current_password: Secret<String>,
+    pub new_password: Secret<String>,
+    pub new_password_check: Secret<String>,
 }
